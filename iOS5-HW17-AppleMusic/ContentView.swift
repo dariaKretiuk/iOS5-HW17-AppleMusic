0a59a21ab10480b2d449d32e3ed6c0d07630b150
dariaKretiuk <kretyukdasha@gmail.com>
2022-04-13 21:46:16 +0700

    Add project

---
+++ iOS5-HW17-AppleMusic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS5-HW17-AppleMusic
+//
+//  Created by Дарья Кретюк on 13.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS5-HW17-AppleMusic/iOS5_HW17_AppleMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS5_HW17_AppleMusicApp.swift
+//  iOS5-HW17-AppleMusic
+//
+//  Created by Дарья Кретюк on 13.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct iOS5_HW17_AppleMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
